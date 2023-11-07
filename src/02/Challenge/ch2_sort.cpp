a03445f9fd53eb0ba1aac984c1ecbfa025aae3dc
Fatemeh Molasarvestani <59932228+Fate-mola@users.noreply.github.com>
2023-11-07 19:54:27 +0000

    Completing sorting an array challenge!

--- src/02/Challenge/ch2_sort.cpp
+++ src/02/Challenge/ch2_sort.cpp
@@ -16,7 +16,25 @@
 // Returns: A boolean value: True on success, false otherwise.
 bool sort_array(int *arr, int n){
 
-    // Write your code here
+    if (arr == NULL)
+        return false;
+    if (n < 2)
+        return true;
+        
+    int temp;
+    // Exchange sort algorithm: ascending
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = i+1; j < n; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 
     return false;
 }
